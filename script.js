b3c8305f378beaa399c376ec368e7d89a2f9c970
Karol Majerczyk <karol.majerczyk@gmail.com>
2024-03-19 07:34:49 +0100

    Create basic structure of the project

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNumber, secondNumber;
+let operator;
+
+function operate(a, sign, b) {
+  if (sign === "+") return add(a, b);
+}
